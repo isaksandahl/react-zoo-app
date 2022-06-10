61b13b02cc312f1b2484f61f542d495ab4758c22
Simon Sandahl <sandahl.simon@hotmail.se>
2022-06-10 13:16:05 +0200

    setup

--- zoo-app/src/App.tsx
+++ zoo-app/src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Layout } from "./components/Layout";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+        <Route index element={<Home />}></Route>
+        <Route path="/animals/:id" element={<Animal />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
