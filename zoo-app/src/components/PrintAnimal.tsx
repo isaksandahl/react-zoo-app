5106d7de3ea2dc8e60bc4f31f0c8b39b6ab471f9
Simon Sandahl <sandahl.simon@hotmail.se>
2022-06-17 12:26:41 +0200

    fixed link issue

--- zoo-app/src/components/PrintAnimal.tsx
+++ zoo-app/src/components/PrintAnimal.tsx
@@ -24,7 +24,7 @@ export const PrintAnimals = (props: IAnimalProps) => {
             onError={(e) => {
               e.currentTarget.onerror = null;
               e.currentTarget.src =
-                "https://shop.peacefulmall.com/no-image.png";
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsxeBa1Yv3CMw17GkzmcayHcu_7-wYHjCdv0f3SZUobD_1CMpBT0FzmyuDoX90t389ASM&usqp=CAU";
             }}
           />
         </Link>
